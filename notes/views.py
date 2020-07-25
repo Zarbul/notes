2fe34c73748d212e37b592abe4805564f7083636
Zarbul <dlyapicem@gmail.com>
2020-07-26 00:16:35 +0300

    create base, list, detail views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
+from notes.models import Note
+
+
+class BaseView(TemplateView):
+    template_name = 'base.html'
+
+
+class NoteListView(ListView):
+    model = Note
+
+
+class NoteDetailView(DetailView):
+    model = Note
