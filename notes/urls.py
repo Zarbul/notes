00f644a6f4a9a3dd9e1b7c2d16614c4973b7eac2
Zarbul <dlyapicem@gmail.com>
2020-07-26 00:19:21 +0300

    create urls list and  detail

---
+++ notes/urls.py
@@ -0,0 +1,23 @@
+"""margin_notes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from .views import BaseView, NoteListView, NoteDetailView
+
+urlpatterns = [
+    path('', BaseView.as_view()),
+    path('note_list/', NoteListView.as_view(), name='home'),
+    path('note_detail/<int:pk>', NoteDetailView.as_view(), name='detail'),
+]
