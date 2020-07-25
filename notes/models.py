25f817584c92b8d690cfacb0076bbadcf4e6d70c
Zarbul <dlyapicem@gmail.com>
2020-07-26 00:17:47 +0300

    create models category and note

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,36 @@
+from django.utils import timezone
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Note(models.Model):
+    name = models.CharField('Название', max_length=150, unique=True)
+    description = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    publish = models.DateField(default=timezone.now())
+    category = models.ManyToManyField(Category)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Записка"
+        verbose_name_plural = "Записки"
+        ordering = ('-publish',)
