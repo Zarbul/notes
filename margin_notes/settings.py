d5b9fee78ed15c58a7b14440c7e7ec075d70c9ef
Zarbul <dlyapicem@gmail.com>
2020-07-31 23:41:53 +0300

    add loginsys app

--- margin_notes/settings.py
+++ margin_notes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
+    'loginsys',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATICFILES_DIRS = [STATIC_DIRS]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
