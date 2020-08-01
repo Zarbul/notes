0ec2e8ce0cc8879a9b2e317b0ef65f9962b10b10
Zarbul <dlyapicem@gmail.com>
2020-08-01 21:23:30 +0300

    LoginRequiredMixin, view notes only auth user

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
@@ -9,8 +11,13 @@ class BaseView(TemplateView):
     template_name = 'base.html'
 
 
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
+    template_name = 'notes/note_list.html'
     model = Note
+
+    def get(self, request, *args, **kwargs):
+        query = Note.objects.filter(author=request.user)
+        return render(request, self.template_name, {'note_list': query})
 
 
 class NoteDetailView(DetailView):
